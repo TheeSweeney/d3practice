e477feb64d7cf1bf3e5f8acdc60965268e891ff6
Brian Sweeney <bsweeney930@gmail.com>
2017-04-19 18:05:59 -0400

    new data set

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
+var data = [
+  {key: "Glazed",   value: 132},
+  {key: "Jelly",    value: 71},
+  {key: "Holes",    value: 337},
+  {key: "Sprinkles",  value: 93},
+  {key: "Crumb",    value: 78},
+  {key: "Chocolate",  value: 43},
+  {key: "Coconut",  value: 20},
+  {key: "Cream",    value: 16},
+  {key: "Cruller",  value: 30},
+  {key: "Éclair",   value: 8},
+  {key: "Fritter",  value: 17},
+  {key: "Bearclaw",   value: 21}
+];
+
 var w = 800;
 var h = 450;
-var data = [132,71,337,93,78,43,20,16,30,8,17,21];
+var margin = {
+  top:20,
+  bottom:20,
+  left:20,
+  right:20
+}
+var width = w - margin.left - margin.left;
+var height = h - margin.top - margin.bottom;
+
 var xScale = d3.scale.linear()
           .domain([0, d3.max(data)])
-          .range([0, w]);
+          .range([0, width]);
 var yScale = d3.scale.linear()
           .domain([0, data.length])
-          .range([0, h])
+          .range([0, height])
 var svg = d3.select('body').append('svg')
             .attr('id', 'chart')
             .attr('width',w)
             .attr('height',h)
+
+var chart = svg.append('g')
+            .classed('display', true)
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
 
 function plot(params){
 
@@ -50,6 +78,6 @@ function plot(params){
       });
 }
 
-plot.call(svg,{
+plot.call(chart, {
   data: data
 })
